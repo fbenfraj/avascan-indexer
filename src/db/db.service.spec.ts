d838f68c98d741e2e062deb406960340092a587a
fbenfraj <farouk.ben-fraj@outlook.com>
2023-03-27 21:52:19 +0200

    Add db service tests

--- src/db/db.service.spec.ts
+++ src/db/db.service.spec.ts
@@ -1,18 +1,45 @@
-import { EntityManager } from '@mikro-orm/postgresql';
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbService } from './db.service';
+import { Block } from 'ethers';
+import { BlockEntity } from '../entities/BlockEntity';
+import {
+  AbstractSqlConnection,
+  AbstractSqlDriver,
+  AbstractSqlPlatform,
+  EntityManager,
+} from '@mikro-orm/postgresql';
+import generateMockBlocks from '../utils/generateMockBlocks';
+import { ConfigService } from '@nestjs/config';
 
 describe('DbService', () => {
   let service: DbService;
+  let entityManagerMock: Partial<EntityManager>;
+
+  const transactionalMock = jest.fn(async (callback) => {
+    await callback({
+      ...entityManagerMock,
+      flush: entityManagerMock.flush,
+    });
+  });
 
   beforeEach(async () => {
-    const entityManagerMock = {
+    entityManagerMock = {
+      create: jest.fn(),
+      persist: jest.fn().mockReturnThis(),
+      flush: jest.fn(),
       fork: jest.fn(),
+      createQueryBuilder: jest.fn(),
+      transactional: transactionalMock as unknown as EntityManager<
+        AbstractSqlDriver<AbstractSqlConnection, AbstractSqlPlatform>
+      >['transactional'],
     };
+
+    (entityManagerMock.fork as jest.Mock).mockReturnValue(entityManagerMock);
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         DbService,
+        ConfigService,
         { provide: EntityManager, useValue: entityManagerMock },
       ],
     }).compile();
@@ -22,5 +49,22 @@ describe('DbService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('saveBlocks', () => {
+    it('should call transactional and persist the given blocks', async () => {
+      const mockBlocks: Block[] = generateMockBlocks(3);
+      const mockBlockEntities = mockBlocks.map(() => new BlockEntity());
+
+      (entityManagerMock.create as jest.Mock).mockImplementation(
+        () => new BlockEntity(),
+      );
+
+      await service.saveBlocks(mockBlocks);
+
+      expect(entityManagerMock.transactional).toHaveBeenCalledTimes(1);
+      expect(entityManagerMock.persist).toHaveBeenCalledTimes(1);
+      expect(entityManagerMock.persist).toHaveBeenCalledWith(mockBlockEntities);
+    });
   });
 });
