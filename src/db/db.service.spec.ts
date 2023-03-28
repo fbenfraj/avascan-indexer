70eed0d96ae97c65b91c6b180d96e5668d936eee
fbenfraj <farouk.ben-fraj@outlook.com>
2023-03-28 12:12:52 +0200

    Add performance test for getTransactionsSortedByValue

--- src/db/db.service.spec.ts
+++ src/db/db.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { DbService } from './db.service';
-import { Block } from 'ethers';
+import { Block, TransactionResponse } from 'ethers';
 import { BlockEntity } from '../entities/BlockEntity';
 import {
   AbstractSqlConnection,
@@ -8,8 +8,9 @@ import {
   AbstractSqlPlatform,
   EntityManager,
 } from '@mikro-orm/postgresql';
-import { generateMockBlocks } from '../utils/mocks';
+import { generateMockBlocks, generateMockTransactions } from '../utils/mocks';
 import { ConfigService } from '@nestjs/config';
+import { TransactionEntity } from '../entities/TransactionEntity';
 
 describe('DbService', () => {
   let service: DbService;
@@ -23,12 +24,22 @@ describe('DbService', () => {
   });
 
   beforeEach(async () => {
+    const queryBuilderMock = {
+      select: jest.fn().mockReturnThis(),
+      orderBy: jest.fn().mockReturnThis(),
+      limit: jest.fn().mockReturnThis(),
+      offset: jest.fn().mockReturnThis(),
+      execute: jest
+        .fn()
+        .mockResolvedValue(Array(10000).fill(new TransactionEntity())),
+    };
+
     entityManagerMock = {
       create: jest.fn(),
       persist: jest.fn().mockReturnThis(),
       flush: jest.fn(),
       fork: jest.fn(),
-      createQueryBuilder: jest.fn(),
+      createQueryBuilder: jest.fn().mockReturnValue(queryBuilderMock),
       transactional: transactionalMock as unknown as EntityManager<
         AbstractSqlDriver<AbstractSqlConnection, AbstractSqlPlatform>
       >['transactional'],
@@ -65,6 +76,28 @@ describe('DbService', () => {
       expect(entityManagerMock.transactional).toHaveBeenCalledTimes(1);
       expect(entityManagerMock.persist).toHaveBeenCalledTimes(1);
       expect(entityManagerMock.persist).toHaveBeenCalledWith(mockBlockEntities);
+    });
+  });
+
+  describe('getTransactionsSortedByValue', () => {
+    it('should query a large number of transactions quickly', async () => {
+      const numberOfTransactions = 100000;
+      const block = generateMockBlocks(1)[0];
+      const transactions: TransactionResponse[] = generateMockTransactions(
+        numberOfTransactions,
+        Number(block.hash),
+      );
+
+      await service.saveTransactions(transactions);
+
+      const startTime: number = Date.now();
+      const result: TransactionEntity[] =
+        await service.getTransactionsSortedByValue('desc', 10000, 0);
+      const endTime: number = Date.now();
+      const duration: number = endTime - startTime;
+
+      expect(duration).toBeLessThan(1000);
+      expect(result.length).toEqual(10000);
     });
   });
 });
