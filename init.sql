b9667472ffac8278d641c383339e36768f768c55
fbenfraj <farouk.ben-fraj@outlook.com>
2023-04-05 15:21:18 +0200

    Fix different primary keys between blocks and transactions and absence of indexes

--- init.sql
+++ init.sql
@@ -1,6 +1,7 @@
 -- initdb/init.sql
 CREATE TABLE IF NOT EXISTS block_entity (
-  hash VARCHAR(66) PRIMARY KEY,
+  id SERIAL,
+  hash VARCHAR(66) UNIQUE,
   number INT,
   timestamp INT,
   parent_hash VARCHAR(66),
@@ -10,14 +11,17 @@ CREATE TABLE IF NOT EXISTS block_entity (
   gas_used NUMERIC,
   miner VARCHAR(42),
   extra_data TEXT,
-  base_fee_per_gas NUMERIC
+  base_fee_per_gas NUMERIC,
+  PRIMARY KEY (id, hash)
 );
 
+CREATE INDEX block_entity_number ON block_entity (number);
+
 CREATE TABLE IF NOT EXISTS transaction_entity (
-  id SERIAL PRIMARY KEY,
+  id SERIAL,
   block_number INT,
   index INT,
-  hash VARCHAR(66),
+  hash VARCHAR(66) UNIQUE,
   type INT,
   "to" VARCHAR(42),
   "from" VARCHAR(42),
@@ -28,5 +32,14 @@ CREATE TABLE IF NOT EXISTS transaction_entity (
   value NUMERIC,
   chain_id NUMERIC,
   block_hash VARCHAR(66),
-  FOREIGN KEY (block_hash) REFERENCES block_entity (hash)
+  FOREIGN KEY (block_hash) REFERENCES block_entity (hash),
+  PRIMARY KEY (id, hash)
 );
+
+CREATE INDEX transaction_entity_block_number ON transaction_entity (block_number);
+
+CREATE INDEX transaction_entity_to ON transaction_entity ("to");
+
+CREATE INDEX transaction_entity_from ON transaction_entity ("from");
+
+CREATE INDEX transaction_entity_block_hash ON transaction_entity (block_hash);
